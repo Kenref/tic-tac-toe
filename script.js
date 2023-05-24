d4a8db731eb2b17b0eb392a36f18eed8e25f9e9a
Kenref <107287396+Kenref@users.noreply.github.com>
2023-05-24 18:29:15 +1000

    set up squares in html

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-const gameBoard = (function () { 
-    
-    let array = []
+//module
+const gameBoard = (function () {
+    "use strict";
 
-})
+    const gameboard = [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    ];
+    
+    function _private() {
+        return gameboard
+    }
+
+
+    return {
+        publicMethod: _private
+    }
+
+
+
+})();
+
+
+// const gameController = (function () {
+
+
+// })();
+
+// //factory
+// function player(playerNumber, symbol) {
+//     return { playerNumber, symbol}
+    
+// }
+
+
+// console.log(gameBoard.sendback())
+gameBoard._private()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -10,3 +61,6 @@ const gameBoard = (function () {
 
 
 // add "and still" and "and new" when the game is over
+
+
+
