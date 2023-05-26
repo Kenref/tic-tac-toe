47c1c870657a0279e9ba658d7dc77afe0f215463
Kenref <107287396+Kenref@users.noreply.github.com>
2023-05-26 17:54:38 +1000

    categorised functions in modules and factorys

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
-//module
+//module -----------------------
 const gameBoard = (function () {
     "use strict";
 
-    const gameboard = [
+    const grid = [
         1, 2, 3,
         4, 5, 6,
         7, 8, 9
     ];
     
-    function _private() {
-        return gameboard
+    function placeMarker(position, marker) {
+        grid.splice(position, 1, marker)
     }
 
 
     return {
-        publicMethod: _private
+        grid,
+        placeMarker
     }
 
 
 
 })();
+//-------------------------
+
+//module ------------------
+const gameController = (function () {
+    function startingPlayer() {
+        if ((Math.floor(Math.random() * 2) + 1) == 1) {
+            let startingPlayer = 1
+            return startingPlayer
+        };
+        let startingPlayer = 2
+        return startingPlayer
+    }
+
+    function switchPlayerTurn() {
+
+    }
 
 
-// const gameController = (function () {
+
+    //check player win: if either player has 123, 456, 789, 147, 258, 369, 159, 357 in any order. try using an array for each player
 
 
-// })();
 
-// //factory
-// function player(playerNumber, symbol) {
-//     return { playerNumber, symbol}
+
+    return {
+        startingPlayer
+    }
+})();
+//---------------------------
+
+//factory----------------------
+function player(playerNumber, symbol) {
+    return { playerNumber, symbol}
     
-// }
-
-
-// console.log(gameBoard.sendback())
-gameBoard._private()
+}
+//----------------------------
 
 
 
-
-
-
-
-
-
-
+// function calls
+console.log(gameBoard.grid)
+console.log(gameController.startingPlayer())
 
 
 
