081fb3b9a3129e91876d33ad0d1c441d8eea95ed
Kenref <107287396+Kenref@users.noreply.github.com>
2023-05-31 12:40:13 +1000

    Player 1 is now always X and always goes first
    refactored code so it is more efficient

--- script.js
+++ script.js
@@ -28,27 +28,27 @@ function playerFactory(playerNumber, marker) {
 
 //module ------------------
 const gameController = (function() {
-    function startingPlayer() {
-        if ((Math.floor(Math.random() * 2) + 1) == 1) {
-            console.log("player 1 is starting")
-            let firstPlayer = 1
-            return firstPlayer
-        } else {
-            console.log("Player 2 is starting")
-            let firstPlayer = 2
-            return firstPlayer
-        }
-    }
+    // function startingPlayer() {
+    //     if ((Math.floor(Math.random() * 2) + 1) == 1) {
+    //         console.log("player 1 is starting")
+    //         let firstPlayer = 1
+    //         return firstPlayer
+    //     } else {
+    //         console.log("Player 2 is starting")
+    //         let firstPlayer = 2
+    //         return firstPlayer
+    //     }
+    // }
 
-    function setActivePlayer(firstPlayer) {
-        if (firstPlayer === 1) {
-            let activePlayer = 1
-            return activePlayer
-        } else {
-            let activePlayer = 2
-            return activePlayer
-        }
-    }
+    // function setActivePlayer(firstPlayer) {
+    //     if (firstPlayer === 1) {
+    //         let activePlayer = 1
+    //         return activePlayer
+    //     } else {
+    //         let activePlayer = 2
+    //         return activePlayer
+    //     }
+    // }
 
     function switchPlayerTurn() {
         if (activePlayer === 1) {
@@ -62,25 +62,26 @@ const gameController = (function() {
 
     // add if there isnt already and o or x there
     function playTurn(location) {
+        currentMarker = getCurrentMarker()
         if (activePlayer == 1) {
             player1.placeMarkerInArray(location)
-            gameBoard.squares[location - 1].textContent = "X"
+            gameBoard.squares[location - 1].textContent = currentMarker
         } else {
             player2.placeMarkerInArray(location)
-            gameBoard.squares[location - 1].textContent = "O";
+            gameBoard.squares[location - 1].textContent = currentMarker;
         }        
         checkWin()
         switchPlayerTurn()
     }
 
-    // function getCurrentMarker() {
-    //     if (activePlayer === 1) {
-    //         activeMarker = "X"
-    //     } else {
-    //         activeMarker = "O"
-    //     }
-    //     return activeMarker
-    // }
+    function getCurrentMarker() {
+        if (activePlayer === 1) {
+            activeMarker = "X"
+        } else {
+            activeMarker = "O"
+        }
+        return activeMarker
+    }
 
     function isNoMoreNumbers() {
         return gameBoard.grid.every(number => typeof number !== "number")
@@ -98,24 +99,18 @@ const gameController = (function() {
             [3, 5, 7],
         ];
         let grid = gameBoard.grid;
-        let winner = false
+        let winner = false;
 
-        // for (let combination of winningNumbers) {
-        //     let [a, b, c] = combination
-        //     if grid[a - 1] == "X"
-        // }
-
-
-
-        for (let i = 0; i < winningNumbers.length; i++) {
-            let [a, b, c] = winningNumbers[i];
-            if (grid[a - 1] === "X" && grid[b - 1] === "X" && grid[c - 1] === "X") {
-                console.log("Player 1 wins!");
-                winner = true
-            }
-            else if (grid[a - 1] === "O" && grid[b - 1] === "O" && grid[c - 1] === "O") {
-                console.log("Player 2 wins!");
-                winner = true
+        for (let combination of winningNumbers) {
+            let [a, b, c] = combination;
+            let currentMarker = getCurrentMarker();
+            if (
+                grid[a - 1] === currentMarker &&
+                grid[b - 1] === currentMarker &&
+                grid[c - 1] === currentMarker
+            ) {
+                console.log(`Player ${activePlayer} Wins`);
+                winner = true;
             }
         }
         if ((isNoMoreNumbers() == true) && (winner == false)) {
@@ -123,11 +118,26 @@ const gameController = (function() {
         }
     }
 
+    //     for (let i = 0; i < winningNumbers.length; i++) {
+    //         let [a, b, c] = winningNumbers[i];
+    //         if (grid[a - 1] === "X" && grid[b - 1] === "X" && grid[c - 1] === "X") {
+    //             console.log("Player 1 wins!");
+    //             winner = true
+    //         }
+    //         else if (grid[a - 1] === "O" && grid[b - 1] === "O" && grid[c - 1] === "O") {
+    //             console.log("Player 2 wins!");
+    //             winner = true
+    //         }
+    //     }
+    //     if ((isNoMoreNumbers() == true) && (winner == false)) {
+    //         console.log("TIE")
+    //     }
+    // }
+
     function placeMarkerOnBoard(e, marker) {
 		if (e.target.textContent !== "X" && e.target.textContent !== "O") {
 			e.target.textContent = marker;
 			playTurn(e.target.classList[1]);
-			console.log(gameBoard.grid);
 		}
 	}
 
@@ -135,11 +145,9 @@ const gameController = (function() {
     for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener("click", function (e) {
             if (activePlayer === 1) {
-                placeMarkerOnBoard(e, "X");
-                console.log(activePlayer)
+                placeMarkerOnBoard(e, getCurrentMarker());
             } else {
-                placeMarkerOnBoard(e, "O");
-                console.log(activePlayer)
+                placeMarkerOnBoard(e, getCurrentMarker());
             }
         });
     }
@@ -147,18 +155,16 @@ const gameController = (function() {
 
 
 
-    let firstPlayer = startingPlayer()
-    let player1 = playerFactory(firstPlayer, "X")
-    let player2 = playerFactory(firstPlayer === 1 ? 2 : 1, "O")
-    let activePlayer = setActivePlayer(firstPlayer)
-
-
-    console.log(gameBoard.grid)
+    let firstPlayer = 1
+    let activePlayer = firstPlayer
+    let player1 = playerFactory(1, "X")
+    let player2 = playerFactory(2, "O")
 
 
 
 
-    //x always goes first
+
+
 
 
 
@@ -168,7 +174,8 @@ const gameController = (function() {
         placeMarkerOnBoard,
         playTurn,
         isNoMoreNumbers,
-        activePlayer
+        activePlayer,
+        getCurrentMarker
         
     }
 })();
